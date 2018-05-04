b16dabba787080540f9a9c49d2de3524c3f4767e
firezenk <firezenk@gmail.com>
2018-05-04 21:53:28 +0200

    Delete old views

--- app/src/main/java/org/firezenk/comicworld/ui/features/character/CharacterView.kt
+++
@@ -1,5 +0,0 @@
-package org.firezenk.comicworld.ui.features.character
-
-import org.firezenk.comicworld.ui.features.commons.Screen
-
-interface CharacterView : Screen<CharacterStates>
--- app/src/main/java/org/firezenk/comicworld/ui/features/characters/CharactersView.kt
+++
@@ -1,5 +0,0 @@
-package org.firezenk.comicworld.ui.features.characters
-
-import org.firezenk.comicworld.ui.features.commons.Screen
-
-interface CharactersView : Screen<CharactersStates>
